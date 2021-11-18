cac8778f3e4352afc9f6a7fe8d03b8e28b9087b2
ISABELA VICTORIA DE NOVAIS ROMANATO <21193062@CFP127ALUNO.LOCAL>
2021-11-18 11:29:07 -0300

    inicio class ControllerPessoa

--- model/controler/ControlerPessoa.php
+++ model/controler/ControlerPessoa.php
@@ -5,7 +5,23 @@ class ControllerPessoa{
     private $_method;
     private $_modelPessoa;
 
-   
+   public function __construct($model)
+   {
+       $this->_modelPessoa = $model;
+       $this->_method = $_SERVER['REQUEST_METHOD'];
+   }
+
+   function router(){
+       switch ($this->_method) {
+           case 'value':
+               # code...
+               break;
+           
+           default:
+               # code...
+               break;
+       }
+   }
 }
 
 ?>
